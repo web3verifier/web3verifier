2782da41ade446c44e70c2900511691a156d963e
web3loginer <web3loginer@proton.me>
2023-03-15 13:57:03 +0000

    test bug fix

--- server/seller/verify_test.sh
+++ server/seller/verify_test.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -eux
 
-./node_modules/.bin/tsc --build --clean
+sed -i "s/web3verifier.com/192.168.15.6:4333/g" src/url.ts
+
+if [ ! -d "node_modules" ]; then
+    npm ci
+fi
 ./node_modules/.bin/tsc
 
 ps aux | grep webpack| grep -v grep | awk '{ print "kill -9", $2 }' | sh
 
 cd ../../client/verify-page/
-npm run dev&
+    if [ ! -d "node_modules" ]; then
+        npm ci
+    fi
+    npm run dev&
 cd ../../server/seller/
 
 sudo node ./src/server_example2.js
