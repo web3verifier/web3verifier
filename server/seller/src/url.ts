745b0115cf5269bdf9ca7f58b04ec8f78a088da0
web3verifier <web3verifier@proton.me>
2023-04-18 16:25:54 +0000

    fix url.ts

--- server/seller/src/url.ts
+++ server/seller/src/url.ts
@@ -1,4 +1,4 @@
-export const SECURITY_SERVER = "https://192.168.15.6:443"
+export const SECURITY_SERVER = "https://web3verifier.com"
 
 //export const SOLANA_BLOCKCHAIN_SERVER  = "https://api.mainnet-beta.solana.com" 
 export const SOLANA_BLOCKCHAIN_SERVER  = "https://solana-mainnet.g.alchemy.com/v2/T7lJiNxz10BQyNLVP9x4T26e7crdnyFU"
