dc00ad9eba262c3a30bab7b4df713101bb0af4a8
web3verifier <web3verifier@proton.me>
2023-06-25 07:08:37 +0000

    remove unused

--- tool/cryptolib.ts
+++
@@ -1,9 +0,0 @@
-
-export abstract class CryptoLib {
-    public abstract createSecretkey(): string 
-    public abstract getPublickeyFromSecret(secretstr: string): string 
-    public abstract sign(plain: string, secretstr: string): string 
-    public abstract verify(plain: string, publickey: string, sign: string): boolean 
-    public abstract hash(str: string): string 
-    public abstract hashFromTicketInfo(dstPublickey: string, priceInt: string, srcPublickey: string): string 
-}
