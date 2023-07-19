6bfb992fab6302bd729271c9f568516cd6403552
web3verifier <web3verifier@proton.me>
2023-07-19 13:02:05 +0000

    fix run.sh

--- server/landing/run.sh
+++ server/landing/run.sh
@@ -1,1 +1,25 @@
-../seller/run.sh
+#!/bin/bash
+set -eux
+
+if [ $# = 1 ]; then
+    Port=$1
+else
+    echo "usage: ./run.sh Port"
+    exit
+fi
+
+if [ ! -d "node_modules" ]; then
+    npm ci
+fi
+
+#./node_modules/.bin/ts-node-dev --respawn --poll ./src/server_example1.ts $Port
+
+cd ./src
+while true; do
+    ps aux | grep "node ./create_session.js" | grep -v grep | awk '{ print "kill -9", $2 }' | sh
+    .././node_modules/.bin/tsc
+    .././node_modules/.bin/node ./create_session.js $Port &
+
+  inotifywait -e modify ./*.ts
+done
+cd ..
