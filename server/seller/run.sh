2279716bd0a2ea993f2a906349f45ec6f98123a8
web3verifier <web3verifier@proton.me>
2023-07-24 03:41:55 +0000

    update

--- server/seller/run.sh
+++ server/seller/run.sh
@@ -18,7 +18,7 @@ while true; do
     ./node_modules/.bin/tsc
     cd ./src
         set +e
-        ps aux | grep "node ./create_session_example.js" | grep -v grep | awk '{ print "sudo kill -9", $2 }' | sh
+        ps aux | grep "node ./create_session.js" | grep -v grep | awk '{ print "sudo kill -9", $2 }' | sh
         set -e
         sudo .././node_modules/.bin/node ./create_session.js $Port &
         inotifywait -e modify ./*.ts
